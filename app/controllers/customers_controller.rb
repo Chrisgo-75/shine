9ef9d6494be67fcc870b49e5bd72d373a7a51d3b
Chris Arndt <carndt@cals.wisc.edu>
2015-11-09 15:55:20 -0600

    Pagination of query results is done and working.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,13 @@
 class CustomersController < ApplicationController
+
+  PAGE_SIZE = 10
+
   def index
     # @customers = Customer.all.limit(10)
+
+    # Look for a new parameter ":page" that indicates which page the user wants, with a
+    #   default of 0.
+    @page = (params[:page] || 0).to_i
 
     if params[:keywords].present?
       @keywords = params[:keywords]
@@ -8,7 +15,8 @@ class CustomersController < ApplicationController
       @customers = Customer.where(
           customer_search_term.where_clause,
           customer_search_term.where_args).
-          order(customer_search_term.order)
+          order(customer_search_term.order).
+          offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
     end
